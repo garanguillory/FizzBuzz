8ef2bbbdba0f2bc2c94c96ea3dcc73ed4b96bf32
Garan Guillory <garanguillory@gmail.com>
2015-03-06 13:13:06 -0600

    Edit index.html and js files

--- js/script.js
+++ js/script.js
@@ -1,33 +1,55 @@
 /*
-The program should print out each number from 1 to 100,
+The program should print out each number from 1 to userNum,
 replacing numbers divisible by both 3 and 5 with “fizz buzz”,
 those divisible by 3 with “fizz”,
 and those divisible by 5 with “buzz”.
 */
 
 $(document).ready(function() {
-  for (var i = 1; i <= 100; i++){
-    if(i % 3 == 0 && i % 5 == 0){
-      $('body').append(" fizz-buzz ");
-    } else if (i % 5 == 0){
-      $('body').append(" buzz ");
-    } else if (i % 3 == 0){
-      $('body').append(" fizz ");
+
+  var message1 = "What is your number?";
+  var message2 = "Enter a number without non-numerical characters.";
+  prompter(message1);
+
+  function check(num){
+    console.log(num);
+    if (isNaN(num)){
+      console.log("That was not a number");
+      prompter(message2);
     } else {
-      $('body').append(" " + i + " ");
+      fizzy(num)
     }
-  };
+  }
+
+  function prompter(msg){
+     userNum = prompt(msg);
+     userNum = parseInt(userNum);
+     check(userNum);
+  }
+
+  function fizzy(num){
+    for (var i = 1; i <= num; i++){
+      if(i % 3 == 0 && i % 5 == 0){
+        $('.fizzbuzz').append(" Fizz-Buzz ");
+      } else if (i % 5 == 0){
+        $('.fizzbuzz').append(" Buzz ");
+      } else if (i % 3 == 0){
+        $('.fizzbuzz').append(" Fizz ");
+      } else {
+        $('.fizzbuzz').append(" " + i + " ");
+      }
+    }
+  }
+  
 });
 
 
-// for (var i = 1; i <= 100; i++){
-//     if(i % 3 == 0 && i % 5 == 0){
-//       console.log("fizz buzz");
-//     } else if (i % 5 == 0){
-//       console.log("buzz");
-//     } else if (i % 3 == 0){
-//       console.log("fizz");
-//     } else {
-//       console.log(i);
-//     }
-//   }
+
+
+
+
+
+
+
+
+
